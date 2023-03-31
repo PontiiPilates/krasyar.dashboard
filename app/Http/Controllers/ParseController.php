3e79236870c17901310445d4852fc215c221bed8
PontiiPilates <s.m.leshukov@gmail.com>
2023-03-31 14:44:33 +0700

    Налажено получение данных для рендеринга

---
+++ app/Http/Controllers/ParseController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ParseController extends Controller
+{
+    /**
+     * Получение csv-файла и преобразование его в массив.
+     */
+    public function convert()
+    {
+        $data = [];
+
+        $unc_path = '//k5/Public/1/';
+        $file_name = 'realtest.csv';
+
+        // Чтение файла
+        $open = fopen($unc_path . $file_name, "r");
+
+        // Сборка массива
+        while ($string = fgetcsv($open, ',')) {
+            $data[] = $string;
+        }
+
+        // Закрытие файла
+        fclose($open);
+
+        echo "<pre>";
+        print_r($data);
+    }
+}
